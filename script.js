6a25965cf84fb9bf25be60de1151f6d4f68ad4d5
RicardoDeca <ricardodeca08@gmail.com>
2022-07-07 09:33:49 -0300

    finished

---
+++ script.js
@@ -0,0 +1,16 @@
+const username = document.querySelectorAll('input')[0]
+        const password = document.querySelectorAll('input')[1]
+        const btn = document.querySelectorAll('button')[3]
+        function handleChange(){
+            if(username.value && password.value.length>=6){
+                btn.classList.add('btnON')
+                btn.classList.remove('btnOFF')
+            }else{
+                btn.classList.add('btnOFF')
+                btn.classList.remove('btnON')
+                
+            }
+        }
+
+        username.addEventListener('input',handleChange)
+        password.addEventListener('input',handleChange)
