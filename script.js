22bc9baf78a536df018f93d29446437b353d2f8e
RicardoDeca <ricardodeca08@gmail.com>
2022-07-08 08:37:33 -0300

    lenght logici

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const username = document.querySelectorAll('input')[0]
         const password = document.querySelectorAll('input')[1]
         const btn = document.querySelectorAll('button')[3]
         function handleChange(){
-            if(username.value && password.value.length>=6){
+            if(username.value && password.value.length>=8){
                 btn.classList.add('btnON')
                 btn.classList.remove('btnOFF')
             }else{
